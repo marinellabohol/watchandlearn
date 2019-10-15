f3e9726fde34cac17bb9b96f07ed9f5d7a28461c
marinellabohol <50226526+marinellabohol@users.noreply.github.com>
2019-10-15 12:21:17 -0700

    Add files via upload

---
+++ watchandlearn/static/watchandlearn/javascript/episode_detail.js
@@ -0,0 +1,8 @@
+ function popUpToggle () {
+    var x = document.getElementById("pop");
+    if (x.innerText === "Popup On") {
+      x.innerText = "Popup Off";
+    } else {
+      x.innerText = "Popup On";
+    }
+  }
