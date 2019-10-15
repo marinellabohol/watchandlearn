41e010256f0653b7a695d30249aa49ee2c8c3738
marinellabohol <50226526+marinellabohol@users.noreply.github.com>
2019-10-15 12:05:03 -0700

    Add files via upload

---
+++ final/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.views.generic import RedirectView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('watchandlearn/', include('watchandlearn.urls')),
+    path('', RedirectView.as_view(url='/watchandlearn/')),
+]
+
+
+# Use static() to add url mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
