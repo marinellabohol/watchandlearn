69ab1adf51524a122d66f21921059b1da7a3dce6
Marinella Bohol <mbohol@berkeley.edu>
2019-10-16 11:27:04 -0700

    cleaning up files

--- watchandlearn/static/watchandlearn/javascript/episode_detail.js
+++
@@ -1,8 +0,0 @@
- function popUpToggle () {
-    var x = document.getElementById("pop");
-    if (x.innerText === "Popup On") {
-      x.innerText = "Popup Off";
-    } else {
-      x.innerText = "Popup On";
-    }
-  }
